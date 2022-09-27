a0455927cd6f67768db8675cf510ea8d8e9e68cd
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-09-27 22:45:30 +0100

    Update products.js

--- glitter/products.js
+++ glitter/products.js
@@ -3,7 +3,7 @@ const products = [
 	{
 		id: 2,
 		logo:'img/logo.png',
-		image: 'img/1.png',
+		image: 'img/1.jpg',
 		
 		title: ' كريم بريق GLITTER للدكتور المصري امير صالح '  ,
 		price1: 5500,
