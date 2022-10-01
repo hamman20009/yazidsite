3ef873e33ea546a8138c61d733752f01f96a5b0c
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-10-01 22:38:05 +0100

    Update products.js

--- maca/products.js
+++ maca/products.js
@@ -4,7 +4,7 @@ const products = [
 		id: 2,
 		logo:'img/logo.png',
 		image: 'img/maca.jpg',
-		image2: 'img/product/zz.jpg'
+		image2: 'img/products/zz.jpg'
 
 		title: 'اقوى حبوب ماكا لتكبير المؤخرة والارداف'  ,
 		price1: 20000,
