488835798f073c6bb084e76301b2843698dda553
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-10-01 01:00:51 +0100

    Update products.js

--- diabete/products.js
+++ diabete/products.js
@@ -5,10 +5,10 @@ const products = [
 		image: 'img/products/1.png',
 		image2: 'img/products/2.png',
 		
-		title: ' العلاج النهائي لتساقط و انبات الشعر    ',
-		price1: 5500,
+		title: ' خلطة الوسيلة لداء السكري ',
+		price1: 6500,
 		price: 3000,
-		description: '  السيروم المكون من 7 زيوت الذي يقضي على الصلع و تساقط الشعر و الشيب المبكر '   ,
+		description: '  خلطة الوسيلة لداء السكري'   ,
 		phone: 0,
 	},
 
