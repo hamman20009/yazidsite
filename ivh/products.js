96ae4ab8e79977e8d0a214cee82e95dd3b710927
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-10-14 01:02:18 +0100

    Update products.js

--- ivh/products.js
+++ ivh/products.js
@@ -5,10 +5,10 @@ const products = [
 		image: 'img/products/12.png',
 		image2: 'img/products/produit.png',
 		
-		title: ' العلاج النهائي لتساقط و انبات الشعر    ',
-		price1: 5500,
-		price: 3000,
-		description: '  السيروم المكون من 7 زيوت الذي يقضي على الصلع و تساقط الشعر و الشيب المبكر '   ,
+		title: ' كبسولات IVH+ للسكري   ',
+		price1: 12500,
+		price: 9500,
+		description: '  كبسولات IVH+ للسكري '   ,
 		phone: 0,
 	},
 
