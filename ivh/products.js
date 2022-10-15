c996085c17687fff98106ae37d9ae5fa7f98d197
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-10-15 08:13:21 +0100

    Update products.js

--- ivh/products.js
+++ ivh/products.js
@@ -6,8 +6,8 @@ const products = [
 		image2: 'img/products/ivh.png',
 		
 		title: ' كبسولات IVH+ للسكري   ',
-		price1: 12500,
-		price: 9500,
+		price1: 9500,
+		price: 5900,
 		description: '  كبسولات IVH+ للسكري '   ,
 		phone: 0,
 	},
