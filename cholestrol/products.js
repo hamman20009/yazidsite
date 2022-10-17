15ba944494c3bac96e0b9c2604bd4513ce29a4ef
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-10-17 13:37:20 +0100

    Update products.js

--- cholestrol/products.js
+++ cholestrol/products.js
@@ -1,16 +1,1 @@
-const products = [
-	{
-		id: 2,
-		logo:'img/logo.png',
-		image: 'img/products/chole.jpg',
-		image2: 'img/products/cholestrol.png',
-		
-		title: ' اقوى منتوج للقضاء على الكوليستول   ',
-		price1: 9500,
-		price: 5500,
-		description: '  اقوى منتوج للقضاء على الكوليستول '   ,
-		phone: 0,
-	},
 
-	
-];
