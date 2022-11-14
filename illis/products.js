bd7c13931aaf925ccef3fe80e6d14ec6485c867f
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-11-14 22:48:26 +0100

    Update products.js

--- illis/products.js
+++ illis/products.js
@@ -2,8 +2,8 @@ const products = [
 	{
 		id: 2,
 		logo:'img/logo.png',
-		image: 'img/products/12.png',
-		image2: 'img/products/produit.png',
+		image: 'img/products/illis.jpg',
+		image2: 'img/products/illis.png',
 		
 		title: ' المرهم رقم واحد للقضاء النهائي على البواسير   ',
 		price1: 5500,
