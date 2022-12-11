ad6435857a3f110e0261441fe50df231fdab080f
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-12-11 14:39:56 +0100

    Update products.js

--- slimming/products.js
+++ slimming/products.js
@@ -1,16 +1,1 @@
-const products = [
-	{
-		id: 2,
-		logo:'img/logo.png',
-		image: 'img/products/Sliming.jpg',
-		image2: 'img/products/Sliming.jpg',
-		
-		title: ' اقوى كبسولات للتخسيس السريع  ',
-		price1: 10000,
-		price: 6900,
-		description: 'اقوى كبسولات للتخسيس السريع تشيان في 30 يوم فقط الان متوفر عندنا و فقط'   ,
-		phone: 0,
-	},
 
-	
-];
