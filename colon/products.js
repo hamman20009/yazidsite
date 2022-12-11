c3caad9090005c668636516376339645b3c3dbfb
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-12-11 14:38:50 +0100

    Update products.js

--- colon/products.js
+++ colon/products.js
@@ -1,16 +1,1 @@
-const products = [
-	{
-		id: 2,
-		logo:'img/logo.png',
-		image: 'img/products/colon.jpg',
-		image2: 'img/products/colonl.png',
-		
-		title: ' افضل و اقوى علاج للقولون    ',
-		price1: 5500,
-		price: 3500,
-		description: '   افضل و اقوى علاج للقولون '   ,
-		phone: 0,
-	},
 
-	
-];
