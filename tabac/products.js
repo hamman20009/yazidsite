1c627e82ec1e6ec8e2966cf82886974c42ec133a
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-12-11 14:40:01 +0100

    Update products.js

--- tabac/products.js
+++ tabac/products.js
@@ -1,16 +1,1 @@
-const products = [
-	{
-		id: 2,
-		logo:'img/logo.png',
-		image: 'img/products/Dukhan.jpg',
-		image2: 'img/products/doukhan.png',
-		
-		title: ' الحل النهائي للتدخين ',
-		price1: 8500,
-		price: 5900,
-		description: '   الحل النهائي للتدخين '   ,
-		phone: 0,
-	},
 
-	
-];
