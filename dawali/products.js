bd83ed0fcd7b65fcd800693c5e872f02fee8f96e
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-12-11 14:38:35 +0100

    Update products.js

--- dawali/products.js
+++ dawali/products.js
@@ -1,16 +1,1 @@
-const products = [
-	{
-		id: 2,
-		logo:'img/logo.png',
-		image: 'img/products/vari.jpg',
-		image2: 'img/products/dawali.png',
-		
-		title: ' العلاج النهائي للدوالي    ',
-		price1: 5500,
-		price: 3500,
-		description: '  العلاج النهائي للدوالي '   ,
-		phone: 0,
-	},
 
-	
-];
