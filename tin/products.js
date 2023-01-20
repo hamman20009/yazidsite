34044576fa9d9437024899c536a9a623b1339fee
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2023-01-20 03:30:51 +0100

    Update products.js

--- tin/products.js
+++ tin/products.js
@@ -5,10 +5,10 @@ const products = [
 		image: 'img/products/tin.jpg',
 		image2: 'img/products/tinl.png',
 		
-		title: ' زيت بذور التين الشوكي     ',
-		price1: 5500,
-		price: 3500,
-		description: '  زيت بذور التين الشوكي   '   ,
+		title: ' القهوة الخضراء للتنحيف السريع   ',
+		price1: 6500,
+		price: 5000,
+		description: '  القهوة الخضراء للتنحيف السريع '   ,
 		phone: 0,
 	},
 
