d1d9c6f23ec871c9e2e224019eb719b681598446
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2023-01-30 00:42:18 +0100

    Update products.js

--- slim/slimming/products.js
+++ slim/slimming/products.js
@@ -2,13 +2,13 @@ const products = [
 	{
 		id: 2,
 		logo:'img/logo.png',
-		image: 'img/products/slimming.jpg',
-		image2: 'img/products/slimming.jpg',
+		image: 'img/products/311338324_1306316610196863_3167089104416476980_n.jpg',
+		image2: 'img/products/cafe.png',
 		
 		title: ' المنتج الاكثر طلبا في الجزائر للتنحيف    ',
-		price1: 7500,
-		price: 5900,
-		description: '  يعتبر Sliming+ أقوى واحسن  منتج  ينجم يضعفك في اسبوع 3 كيلو واكثر !!! تكوبي لابيتي ودخل كرش وتنحي ليڨاز وتقلاق ومهدئة الاعصاب يزيل الشحم  '   ,
+		price1: 12500,
+		price: 9500,
+		description: '  يعتبر green cofee أقوى واحسن  منتج  ينجم يضعفك في اسبوع 3 كيلو واكثر !!! تكوبي لابيتي ودخل كرش وتنحي ليڨاز وتقلاق ومهدئة الاعصاب يزيل الشحم  '   ,
 		phone: 0,
 	},
 
