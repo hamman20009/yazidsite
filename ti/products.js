3d27b2353c4192b4a057f8d678ab0097e5acdb81
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2023-02-23 15:24:11 +0100

    Update products.js

--- ti/products.js
+++ ti/products.js
@@ -2,13 +2,13 @@ const products = [
 	{
 		id: 2,
 		logo:'img/logo.png',
-		image: 'img/products/12.png',
-		image2: 'img/products/produit.png',
+		image: 'img/products/TITAN1.jpg',
+		image2: 'img/products/titan.png',
 		
-		title: ' العلاج النهائي لتساقط و انبات الشعر    ',
-		price1: 5500,
-		price: 3000,
-		description: '  السيروم المكون من 7 زيوت الذي يقضي على الصلع و تساقط الشعر و الشيب المبكر '   ,
+		title: ' TITAN+ GEL ORIGINAL   ',
+		price1: 15500,
+		price: 7900,
+		description: ' titan gel orignal '   ,
 		phone: 0,
 	},
 
