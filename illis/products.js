4a518c82c460ebd421bcbc6cb10f64628e99a0e9
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2023-02-24 01:54:28 +0100

    Update products.js

--- illis/products.js
+++ illis/products.js
@@ -1,1 +1,17 @@
+const products = [
+	{
+		id: 2,
+		logo:'img/logo.png',
+		image: 'img/products/dawli.jpg',
+		image2: 'img/products/dawali.png',
+		
+		title: ' زيت الدوالي    ',
+		price1: 5500,
+		price: 3900,
+		description: '  زيت الدوالي'   ,
+		phone: 0,
+	},
+
+	
+];
 
