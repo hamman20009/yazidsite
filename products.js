8f269959f1bc45d7e64c823326ab08ceda896cdb
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2023-06-21 23:42:24 +0100

    Update products.js

--- products.js
+++ products.js
@@ -2,7 +2,7 @@ const products = [
 	{
 		id: 2,
 		logo:'img/logo.png',
-		image: 'img/products/photo_2023-05-20_09-33-12.jpg',
+		image: 'img/products/cr1.png',
 		image2: 'img/products/produit.png',
 		
 		title: ' العلاج النهائي لتساقط و انبات الشعر    ',
