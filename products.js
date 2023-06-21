75367eae10c4520c647e7154e115f690644127a7
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2023-06-22 00:28:51 +0100

    Update products.js

--- products.js
+++ products.js
@@ -3,7 +3,7 @@ const products = [
 		id: 2,
 		logo:'img/logo.png',
 		image: 'img/products/cr1.png',
-		image2: 'img/products/cr12.png',
+		image2: 'img/products/cr12.jpg',
 		
 		title: ' العلاج النهائي لتساقط و انبات الشعر    ',
 		price1: 5500,
