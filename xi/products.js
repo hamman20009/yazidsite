fa8ab89d60b950aa3ca4b466797ae352221e6a13
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2023-09-29 02:12:40 +0100

    Update products.js

--- xi/products.js
+++ xi/products.js
@@ -4,7 +4,8 @@ const products = [
 		logo:'img/logo.png',
 		image: 'img/products/xi.jpg',
 		image2: 'img/products/xi2.jpg',
-		
+		image3: 'img/products/xi3.jpg',
+
 		title: ' ساعة Xioami الذكية Smart band 7   ',
 		price1: 11000,
 		price: 7900,
