fc4d39ce5c9857ef22b074b112eb1f66efef2e27
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2024-02-04 19:13:54 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,16 @@
+const products = [
+  {
+    id: 2,
+    logo:'img/logo.png',
+    image: 'img/products/cr12.jpg',
+    image2: 'img/products/lc.png',
+    
+    title: ' العلاج النهائي لتساقط و انبات الشعر    ',
+    price1: 5500,
+    price: 3900,
+    description: '   هار لوشن القاضي على الصلع و الشيب المبكر '   ,
+    phone: 0,
+  },
 
+  
+];
