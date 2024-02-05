9ab05bc4b1d442fc5f1e10f5c293009f8f10207f
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2024-02-05 02:55:17 +0100

    Update products.js

--- diabete/products.js
+++ diabete/products.js
@@ -2,7 +2,7 @@ const products = [
 	{
 		id: 2,
 		logo:'img/logo.png',
-		image: 'img/products/WhatsApp-Image-2022-10-16-at-21.57.33.jpeg',
+		image: 'img/hyhy.jpg',
 		image2: 'img/products/Picsart_23-10-15_20-31-01-294.png',
 		
 		title: ' glousamine لعلاج عرق النسا و الروماتيزم و الام المفاصل  ',
