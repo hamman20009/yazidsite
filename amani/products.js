b0cb78a371ba497a7b1bea4cbd5ab0dee8349fc3
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2024-02-07 12:44:27 -0500

    Update products.js

--- amani/products.js
+++ amani/products.js
@@ -5,9 +5,9 @@ const products = [
 		image: 'img/products/12.jpg',
 		image2: 'img/products/produit.jpg',
 		
-		title: '    السعر مليون و نصف COLOCOFORT لعلاج القولون العصبي  ',
+		title: '               مليون و متين              COLOCOFORT لعلاج القولون العصبي  ',
 		price1: 18000,
-		price: 15000 ,
+		price: 12000 ,
 		description: '  العلاج الامريكي ضد القولون العصبي نقدم كل الضمانات على جودة و فعالية المنتج  '   ,
 		phone: 0554139163,
 	},
