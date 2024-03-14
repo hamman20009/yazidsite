06462a1e3de4bb500f763c27cf9eb21d51442109
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2024-03-14 17:00:22 +0100

    Update products.js

--- rosemary/products.js
+++ rosemary/products.js
@@ -2,7 +2,7 @@ const products = [
   {
     id: 2,
     logo:'img/logo.png',
-    image: 'img/products/cr12.jpg',
+    image: 'img/products/rose.jpg',
     image2: 'img/products/newl.png',
     
     title: ' العلاج النهائي لتساقط و انبات الشعر    ',
