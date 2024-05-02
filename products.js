d3864b9c6c76eb1286cbf1e2bada7e1cb0b757b4
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2024-05-02 21:33:49 +0100

    Update products.js

--- products.js
+++ products.js
@@ -3,11 +3,11 @@ const products = [
     id: 2,
     logo:'img/logo.png',
     image: 'img/products/cr12.jpg',
-    image2: 'img/products/newl.png',
+    image2: '',
     
     title: ' العلاج النهائي لتساقط و انبات الشعر    ',
     price1: 6500,
-    price: 4300,
+    price: 4500,
     description: '   سيروم انبات فراغات  الشعر و اللحية  المثبت علميا '   ,
     phone: 0,
   },
