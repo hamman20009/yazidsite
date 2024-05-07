2ff07ebc5c4c74f4efd147624b42eae546e643e4
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2024-05-07 23:47:25 +0100

    Update products.js

--- rosemary/products.js
+++ rosemary/products.js
@@ -2,12 +2,12 @@ const products = [
   {
     id: 2,
     logo:'img/logo.png',
-    image: 'img/products/rose.jpg',
+    image: 'img/products/une-personne-atteinte-de-calvitie.jpg',
     image2: 'img/products/newl.png',
     
     title: ' العلاج النهائي لتساقط و انبات الشعر    ',
-    price1: 6500,
-    price: 4300,
+    price1: 0,
+    price: 0,
     description: '   سيروم انبات فراغات  الشعر و اللحية  المثبت علميا '   ,
     phone: 0,
   },
