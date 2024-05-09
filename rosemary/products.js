bed0df3004c0ef7b98bcf8454e4ecd78857fb35b
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2024-05-09 02:49:10 +0100

    Update products.js

--- rosemary/products.js
+++ rosemary/products.js
@@ -3,7 +3,7 @@ const products = [
     id: 2,
     logo:'img/logo.png',
     image: 'img/products/une-personne-atteinte-de-calvitie.jpg',
-    image2: 'img/products/newl.png',
+    
     
     title: ' العلاج النهائي لتساقط و انبات الشعر    ',
     price1: 0,
